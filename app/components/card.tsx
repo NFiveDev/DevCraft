5e2c3cfea7452b61a56374656f2c459546e8ed73
NFiveDev <nmhjort7@gmail.com>
2024-03-10 22:55:29 +0100

    working on card

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,5 +1,13 @@
-
-
 export function Card() {
-    return <></>
+    return (
+        <div className="p-5 h-64 shadow-lg rounded-lg bg-gray-50">
+            <div className="flex flex-row  w-full items-center">
+                <h3 className="flex-1 text-2xl font-semibold">Python Beginner</h3>
+                <img className="h-12 w-12" src="/icons/course/python.svg" alt="" />
+            </div>
+            <div className="pt-2">
+                <p className=" text-base text-slate-500">If you’re looking for a programming language that’s flexible and easy to read, try learning Python. It’s one of the most popular languages today, and programming in Python is used for everything from web and software development to data science and analytics to quality assurance.</p>
+            </div>
+        </div>
+    );
 }
--- app/routes/course.tsx
+++ app/routes/course.tsx
@@ -1,25 +1,33 @@
-import { Card } from "~/components/card";
-import { FilterLink } from "~/components/link";
+import { Card } from '~/components/card';
+import { FilterLink } from '~/components/link';
 
 export default function Course() {
-    return <>
-    <section>
-        <h1 className=" text-5xl font-bold text-slate-900 pb-3">Explore the courses and choose one right for you!</h1>
-        <h4 className="text-slate-500 text-base">The Web Development courses we offer are designed to help prepare you for a career in front-end and back-end development, full-stack engineering, and web design, providing you with comprehensive skills to build and maintain dynamic websites and applications.</h4>
-    </section>
+    return (
+        <>
+            <section>
+                <h1 className=" text-5xl font-bold text-slate-900 pb-3">
+                    Explore the courses and choose one right for you!
+                </h1>
+                <h4 className="text-slate-500 text-base">
+                    The Web Development courses we offer are designed to help prepare you for a
+                    career in front-end and back-end development, full-stack engineering, and web
+                    design, providing you with comprehensive skills to build and maintain dynamic
+                    websites and applications.
+                </h4>
+            </section>
 
-    <section className="mt-5">
-        <div className="flex flex-row gap-x-3">
-            <FilterLink title="Topic" path="/course" query="?CourseType=topic"  />
-            <FilterLink title="Project" path="/course" query="?CourseType=project"  />
-        </div>
+            <section className="mt-8">
+                <div className="flex flex-row gap-x-3">
+                    <FilterLink title="Topic" path="/course" query="?CourseType=topic" />
+                    <FilterLink title="Project" path="/course" query="?CourseType=project" />
+                </div>
 
-        <div className="grid grid-cols-3 gap-4">
-        <div className=" col-span-3 lg:col-span-1">
-            <Card />
-        </div>
-        </div>
-        
-    </section>
-    </>
+                <div className="grid grid-cols-3 gap-4 mt-3">
+                    <div className=" col-span-3 lg:col-span-1">
+                        <Card />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
