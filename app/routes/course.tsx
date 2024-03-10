7c984e7d5c7cf8d2f6391c9edd07c4e4f7866155
NFiveDev <nmhjort7@gmail.com>
2024-03-10 01:35:07 +0100

    course page

--- app/routes/course.tsx
+++ app/routes/course.tsx
@@ -1,12 +1,16 @@
+import { Card } from "~/components/card";
+
 export default function Course() {
     return <>
     <section>
-        <h1 className=" text-4xl font-semibold text-slate-900 pb-3">Explore a wide range of courses and pick the one that aligns with your goals</h1>
+        <h1 className=" text-4xl font-bold text-slate-900 pb-2">Explore a wide range of courses and pick the one that aligns with your goals</h1>
         <h4 className="text-slate-500 text-base">The Web Development courses we offer are designed to help prepare you for a career in front-end and back-end development, full-stack engineering, and web design, providing you with comprehensive skills to build and maintain dynamic websites and applications.</h4>
     </section>
 
     <section className="grid grid-cols-3 gap-4">
-        
+        <div className=" col-span-3 lg:col-span-1">
+            <Card />
+        </div>
     </section>
     </>
 }
