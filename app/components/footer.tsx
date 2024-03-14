8ae50504d56800b7a9b9b59060a6ff07c4a1582e
NFiveDev <nmhjort7@gmail.com>
2024-03-15 00:11:15 +0100

    working on footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,15 @@
 export default function Footer() {
-    return (<footer>
-        <h1>footer</h1>
+    return (
+    
+    <footer className=" bg-slate-800">
+        <div className="flex justify-between py-2 max-w-screen-xl mx-auto px-3 2xl:px-0 ">
+            <div className="justify-end">
+                <h1>footer</h1>
+            </div>
+            <div className="justify-end">
+                <h1>footer</h1>
+            </div>
+        </div>
+        
     </footer>)
 }
