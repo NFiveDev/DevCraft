698a4247063fd8cece24bbc78332252c3f930d1f
NFiveDev <nmhjort7@gmail.com>
2024-03-17 21:26:20 +0100

    password validator

--- app/lib/utility/validators.ts
+++ app/lib/utility/validators.ts
@@ -6,3 +6,11 @@ export const validateEmail = (mail: string | undefined | null) => {
     });
     return emailSchema.safeParse(mail).success;
 };
+
+export const validatePassword = (password: string | undefined | null) => {
+    const passwordSchema = z.object({
+        password: z.string().min(8).max(16)
+    })
+
+    return passwordSchema.safeParse(password).success;
+}
